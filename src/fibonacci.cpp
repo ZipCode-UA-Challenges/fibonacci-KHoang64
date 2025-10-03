d32e26379cf298622992e142e801d5699207c786
Kent ong <50777480+KHoang64@users.noreply.github.com>
2025-10-03 19:11:02 +0000

    the original contract is nice that its elegntly put, but i just edited it so its simple for mi

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -15,15 +15,28 @@ void fibonacciRacer(std::array<uint64_t, MAX_64_BIT_FIBONACCI_INDEX + 1>& result
      */
 }
 
+
 uint64_t fibonacci(int n) {
-    /**
-     * @todo Implement the fibonacci function declared in include/fibonacci.hpp.
-     */
-     
-     // dummy value
-     return 42;
     
-    return -1;
+
+    uint64_t prev = 0, curr = 1;
+    if (n == 0)return prev;
+    if(n == 1) return curr;
+    /*
+    // else remmber that      
+    prev = F(n-2)
+    curr = F(n-1) 
+     */
+    //now for,when n is  2 or more, start at 2 
+    for(int i = 2; i <= n; i++){
+
+        uint64_t next = prev + curr; // F(n) = F(n-1) + F(n-2)
+        prev = curr; // move prev to F(n-1)
+        curr = next; // move curr to F(n)
+    }
+    return curr;
+
+     
 }
 
 } // namespace fibonacci
